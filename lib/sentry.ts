95ca22a7d22b5088edeadf3dd493d89874250833
larbisahli <larbisahli1905@gmail.com>
2023-02-13 22:12:34 +0100

    work

--- lib/sentry.ts
+++ lib/sentry.ts
@@ -1,15 +1,9 @@
-import * as Sentry from '@sentry/nextjs';
-
 interface LogsType {
   message: string | null;
   error: Error | string;
 }
 
 export function sentry({ message, error }: LogsType) {
-  if (process.env.NODE_ENV === 'production') {
-    Sentry.captureException(error);
-    return;
-  }
   console.error(`<==: ${message} :==>`);
   if (message === 'graphQLClient.request') {
     console.error(JSON.stringify(error, undefined, 2));
